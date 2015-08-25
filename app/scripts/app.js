5c71be2baf501d4d0fb82cb06d40a67a06126686
Fazrin <fazrin.mutaqin@wgs.co.id>
2015-08-25 13:52:37 +0700

    xtForm validate example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,4 +11,41 @@ var app = angular.module('angularTrainingApp', [
   'ui.router',
   'angulike',
   'xtForm'
-  ]); 
+  ]).config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/main.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .state('blog', {
+      url: '/blog',
+      templateUrl: 'views/blog.html',
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'views/contact.html',
+    })
+    .state('blog.detail', {
+      url: '/detail',
+      templateUrl: 'views/blog-detail.html',
+      controller : 'SocmedCtrl'
+    })
+    .state('blog.fazrin', {
+      url: '/fazrin',
+      templateUrl: 'views/blog-fazrin.html',
+      controller : 'BlogfazrinCtrl'
+    })
+    .state('blog.sigit', {
+      url: '/sigit',
+      templateUrl: 'views/blog-sigit.html',
+      controller : 'SocmedCtrl'
+    });       
+});
